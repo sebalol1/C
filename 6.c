d924f5764f480738078bf45afb191c3f5d4c96fe
Sebastian Zielaskiewicz <szielaskiewicz@sigma.ug.edu.pl>
2014-11-01 21:11:00 +0100

    trzecie podejscie

--- 6.c
+++ 6.c
@@ -3,9 +3,22 @@
 #include<time.h>
 int main()
 {
-int i;
-i=rand()%5+1;
-printf("Wylosowałeś liczbe %d9 \n" , i);
+int k;
+int u;
+
+    srand(time(0));
+    k=rand()%5+1;
+printf("Podaj liczbę , może zgadniesz \n");
+scanf("%i", &u);
+    
+
+    if(k==u)
+	{printf("Brawo \n");}
+    if(k==u+1 || k==u-1)
+	{printf("Blisko \n ");}
+    else
+	{printf("Próbuj jeszcze raz \n");}
+
 
 
 
