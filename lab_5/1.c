97577de2309c595e6370a87fd6324abf1b83ca8b
sebalol1 <szielaskiewicz@sigma.ug.edu.pl>
2014-11-29 18:47:30 +0100

    Update 1.c

--- lab_5/1.c
+++ lab_5/1.c
@@ -1,30 +1,21 @@
-/*Zadanie 1.Napisz program, ktory deklaruje:
-a) stala zmiennoprzecinkowa PI = 3.1415926535
-b) staly lancuch znakow Przedmiot, ktory zawiera napis "programowanie"
-c) zmienna Rok, ktora jest liczba calkowita dodatnia, sprobuj ustawic
-ja na -1, a nastepnie wyswietl jej zawartosc
-d) wyswietl ilosc bajtow, ktora jest potrzebna do zadeklarowania
-zmiennej typu char, short, int oraz long.*/
 
 #include <stdio.h>
 
 int main()
 {
-  //a) stala zmiennoprzecinkowa PI = 3.1415926535
+  
   long double PI = 3.1415926535;
   printf("PI= %.10Lf\n", PI);
 
-  //b) staly lancuch znakow Przedmiot, ktory zawiera napis "programowanie"
+  
   char Przedmiot[]="programowanie";
   printf("%s\n", Przedmiot);
 
-  //c) zmienna Rok, ktora jest liczba calkowita dodatnia, sprobuj ustawic
-  //ja na -1, a nastepnie wyswietl jej zawartosc
+ 
   unsigned Rok=-1;
   printf("%d\n", Rok);
 
-  //d) wyswietl ilosc bajtow, ktora jest potrzebna do zadeklarowania
-  //zmiennej typu char, short, int oraz long.*/
+  
   char a;
   short b;
   int c;
