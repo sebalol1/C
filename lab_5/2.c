b5f3f6c7abf9497a768030a8b2f5d06217991b75
sebalol1 <szielaskiewicz@sigma.ug.edu.pl>
2014-11-29 18:48:04 +0100

    Update 2.c

--- lab_5/2.c
+++ lab_5/2.c
@@ -1,13 +1,3 @@
-/*Zadanie 2.Napisz program, ktory pyta uzytkownika o numer miesiaca, a nastepnie
-deklaruje zmienna PORA_ROKU, ktora ustawia na:
-a) WIOSNA, jesli numer miesiaca to 3,4 lub 5
-b) LATO, jesli numer miesiaca to 6,7 lub 8
-c) JESIEN, jesli numer miesiaca to 9,10 lub 11
-d) ZIMA, jesli numer miesiaca to 12,1 lub 2
-W tym celu zadeklaruj zmienna wyliczeniowa PORA_ROKU, ktora zawiera
-pola WIOSNA, LATO, JESIEN i ZIMA.
-Nastepnie sprawdz, czy PORA_ROKU to ZIMA, a jesli tak, to niech
-program wypisze komunikat "Zima zima zima".*/
 
 #include <stdio.h>
 
